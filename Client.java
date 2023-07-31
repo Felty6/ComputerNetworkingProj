064180fe1d80c856c51241dd64cb61d0eb74b660
Rio Taiga <53506239+Felty6@users.noreply.github.com>
2023-07-30 19:16:38 -0700

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,108 +1,120 @@
+package ComputerNetworkingProj;
+
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
-    private static final int MAX_SEQUENCE_NUMBER = 65536; // 2^16, maximum sequence number
-    private static final int INITIAL_WINDOW_SIZE = 1;
-    private static final int MAX_WINDOW_SIZE = 216;
+	private static final int MAX_SEQUENCE_NUMBER = 65536; // maximum sequence number
+	private static final int INITIAL_WINDOW_SIZE = 1; // sliding window initial
+	private static final int MAX_WINDOW_SIZE = 216; // sliding window final
 
-    private DatagramSocket clientSocket;
-    private InetAddress serverAddress;
-    private int serverPort;
+	private DatagramSocket clientSocket;
+	private InetAddress serverAddress;
+	private int serverPort;
 
-    public Client(String serverIP, int serverPort) throws SocketException, UnknownHostException {
-        clientSocket = new DatagramSocket();
-        serverAddress = InetAddress.getByName(serverIP);
-        this.serverPort = serverPort;
-    }
+	// store window size and sequence numbers history
+	private List<Integer> windowSizeHistory = new ArrayList<>();
+	private List<Integer> sentSeqNumHistory = new ArrayList<>();
 
-    public void start() throws IOException {
-        // Send the initial string to the server
-        byte[] sendData = "network".getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
-        clientSocket.send(sendPacket);
+	public Client(String serverIP, int serverPort) throws SocketException, UnknownHostException {
+		clientSocket = new DatagramSocket();
+		serverAddress = InetAddress.getByName(serverIP);
+		this.serverPort = serverPort;
+	}
 
-        byte[] receiveData = new byte[1024];
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        clientSocket.receive(receivePacket);
-        String receivedData = new String(receivePacket.getData()).trim();
+	public void start() throws IOException {
+		// Send the initial string to the server
+		byte[] sendData = "network".getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+		clientSocket.send(sendPacket);
 
-        if (receivedData.equals("Connection setup success")) {
-            System.out.println("Connection established with server: " + serverAddress + ":" + serverPort);
+		byte[] receiveData = new byte[1024];
+		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		clientSocket.receive(receivePacket);
+		String receivedData = new String(receivePacket.getData()).trim();
 
-            // Start sending data segments
-            sendDataSegments();
-        }
+		if (receivedData.equals("Connection setup success")) {
+			System.out.println("Connection established with server: " + serverAddress + ":" + serverPort);
 
-        clientSocket.close();
-    }
+			// Start sending data segments
+			sendDataSegments();
+		}
 
-    private void sendDataSegments() throws IOException {
-        int sequenceNumber = 0;
-        int sentSegments = 0;
-        int receivedAcks = 0;
-        int windowSize = INITIAL_WINDOW_SIZE;
+		clientSocket.close();
+	}
 
-        while (sentSegments < 10000000) {
-            if (sentSegments % 1024 == 0) {
-                // Simulate segment loss by not sending every 1024th segment
-                if (Math.random() < 0.2) {
-                    System.out.println("Segment loss: " + sequenceNumber);
-                    sequenceNumber++;
-                    continue;
-                }
-            }
+	private void sendDataSegments() throws IOException {
+		int sequenceNumber = 0;
+		int sentSegments = 0;
+		int receivedAcks = 0;
+		int windowSize = INITIAL_WINDOW_SIZE;
 
-            String segment = String.valueOf(sequenceNumber);
-            byte[] sendData = segment.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
-            clientSocket.send(sendPacket);
+		while (sentSegments < 10000000) {
+			if (sentSegments % 1024 == 0) {
+				// Simulate segment loss by not sending every 1024th segment
+				if (Math.random() < 0.2) {
+					System.out.println("Segment loss: " + sequenceNumber);
+					sequenceNumber++;
+					continue;
+				}
+			}
 
-            byte[] receiveData = new byte[1024];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
-            String receivedData = new String(receivePacket.getData()).trim();
+			String segment = String.valueOf(sequenceNumber);
+			byte[] sendData = segment.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+			clientSocket.send(sendPacket);
 
-            if (receivedData.startsWith("ACK")) {
-                int ackSeqNum = Integer.parseInt(receivedData.substring(4));
-                if (ackSeqNum == sequenceNumber + 1) {
-                    sequenceNumber++;
-                    receivedAcks++;
-                }
-            }
+			byte[] receiveData = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
 
-            if (sentSegments % 1024 == 0 || receivedAcks == windowSize) {
-                // Sliding window adjustment
-                if (windowSize < MAX_WINDOW_SIZE) {
-                    windowSize *= 2;
-                }
+			String receivedData = new String(receivePacket.getData()).trim();
 
-                if (receivedAcks == windowSize) {
-                    receivedAcks = 0;
-                }
-            }
+			// Split the receivedData by whitespaces to handle any potential leading/trailing spaces
+			String[] dataParts = receivedData.split("\\s+");
+			if (dataParts[0].equals("ACK")) {
+				int ackSeqNum = Integer.parseInt(dataParts[1]);
+				if (ackSeqNum == sequenceNumber + 1) {
+					sequenceNumber++;
+					receivedAcks++;
+				}
+			}
 
-            sentSegments++;
+			if (sentSegments % 1024 == 0 || receivedAcks == windowSize) {
+				// Sliding window adjustment
+				if (windowSize < MAX_WINDOW_SIZE) {
+					windowSize *= 2;
+				}
 
-            if (sentSegments % 1000 == 0) {
-                double goodPut = (double) sentSegments / (sentSegments - receivedAcks);
-                System.out.println("Sent segments: " + sentSegments +
-                                   ", Received ACKs: " + receivedAcks +
-                                   ", Window size: " + windowSize +
-                                   ", Good-put: " + goodPut);
-            }
-        }
-    }
+				if (receivedAcks == windowSize) {
+					receivedAcks = 0;
+				}
+			}
 
-    public static void main(String[] args) {
-        String serverIP = "SERVER_IP_ADDRESS"; // Replace with the IP address of the server computer
-        int serverPort = 12345; // Change this to the server's port
+			sentSegments++;
 
-        try {
-            Client client = new Client(serverIP, serverPort);
-            client.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			// store the window size
+			windowSizeHistory.add(windowSize);
+			if (sentSegments % 1000 == 0) {
+				// goodput formula
+				double goodPut = (double) sentSegments / (sentSegments - receivedAcks);
+				System.out.println("Sent segments: " + sentSegments + ", Received ACKs: " + receivedAcks
+						+ ", Window size: " + windowSize + ", Good-put: " + goodPut);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String serverIP = "192.168.1.123"; // IP address of the server computer
+		int serverPort = 6463; // Input server's port
+
+		try {
+			Client client = new Client(serverIP, serverPort);
+			client.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
